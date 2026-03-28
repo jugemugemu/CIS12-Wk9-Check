cec575f3af815bb590f312753a50268fa79ea11e
Jugemugemu <morinsagan@gmail.com>
2026-03-27 21:37:55 -0700

    Bug fix

--- numberguesser.py
+++ numberguesser.py
@@ -4,8 +4,8 @@ def dumberguesser():
 
     input("Think of a number between 1 and 100, inclusive, and hit ENTER. I'm going to try to guess it.")
     while True:
-        response = input(f'Is it {guess} [Y/n]?').lower()[0] or 'y' # Defaults to y (yes, you have to type 'n' each time it's wrong)
-        correct = responses.get(response, True) # If not y or n, default to True (y)
+        response = input(f'Is it {guess} [Y/n]?').lower() or 'y' # Defaults to y (yes, you have to type 'n' each time it's wrong)
+        correct = responses.get(response[0], True) # If not y or n, default to True (y)
         if correct:
             print(f'I knew it was {guess}!')
             break
